0e7081b392abac2f74f2121c8bfb0d34dd26368f
JeongHee <s2wjdgml@gamil.com>
2020-04-14 17:41:15 +0900

    paint color

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-console.log("hello world!");
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName("jsColor");
+
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function startPainting() {
+    painting = true;
+}
+
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    console.log(painting);
+    if (!painting) {
+        // console.log("creating path in", x, y);
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        // console.log("creating line in", x, y);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    //strolStyle을 오버라이딩
+}
+
+if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+//color 는 변수명 photato 도 가능하고 아무거나 써도 ok
